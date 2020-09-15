eed695abf488850c5621d2131b4aa528067245e2
Kostya Petrov <KostyaPetrov1986@gmail.com>
2020-09-15 20:31:11 +0400

    commit1

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat {
+
+    private String mewing;
+
+    public String getMewing() {
+        return mewing;
+    }
+
+    public void setMeow(String mewing) {
+        this.mewing = mewing;
+    }
 }
